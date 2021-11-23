ca886c6f617066e943ee13b90966876ee2ee7659
Katherine Roll <kroll256@gmail.com>
2021-11-22 21:45:31 -0500

    commit tree 97f25df720d8f7c9c2a69d1847372c6fa0934c5f

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+let num1 = 1
+
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'not bob';
+}
+
